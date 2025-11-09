d60ad309309b45db956b83577e33478444215e55
ibiscum <ulf.luecking@gmail.com>
2025-11-09 12:43:10 +0100

    ch03 3_2

--- chapter-03/3_2/src/App.vue
+++ chapter-03/3_2/src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id='app'>
-    <current-time class='col-4' />
-    <task-input class='col-6' />
+  <div id="app">
+    <current-time class="col-4" />
+    <task-input class="col-6" />
   </div>
 </template>
 
 <script>
-import CurrentTime from './components/CurrentTime.vue';
-import TaskInput from './components/TaskInput.vue';
+import CurrentTime from "./components/CurrentTime.vue";
+import TaskInput from "./components/TaskInput.vue";
 
 export default {
-  name: 'TodoApp',
+  name: "TodoApp",
   components: {
     CurrentTime,
     TaskInput,
